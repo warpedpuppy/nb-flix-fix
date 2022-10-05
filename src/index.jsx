602c69dcb182ee6f5ebbe519bd1b93bd8e0a7620
nickbeasley <109631653+nickbeasley@users.noreply.github.com>
2022-10-05 18:20:07 -0500

    Create index.jsx
    creating file while importing react

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.scss";
+
+class MyFlixApplication extends React.Component {
+  render() {
+    return (
+      <div className="my-flix">
+        <div>Good morning</div>
+      </div>
+    );
+  }
+}
+
+// Finds the root of your app
+const container = document.getElementsByClassName("app-container")[0];
+
+// Tells React to render your app in the root DOM element
+ReactDOM.render(React.createElement(MyFlixApplication), container);
