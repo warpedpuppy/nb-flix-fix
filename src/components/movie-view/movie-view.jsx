e1668b4ebe4d51c7e118e1c658d72bf3c0c2425f
nickbeasley <109631653+nickbeasley@users.noreply.github.com>
2022-10-14 13:49:45 -0500

    Update movie-view.jsx
    updated with bootstrap

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Button, Col, Row } from "react-bootstrap";
 
 import "./movie-view.scss";
 
@@ -17,36 +18,65 @@ export class MovieView extends React.Component {
 
     return (
       <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} />
-        </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
-        <div className="movie-genre">
-          <span className="label">Genre: </span>
-          <span className="value">{movie.Genre}</span>
-        </div>
-        <div className="movie-director">
-          <span className="label">Director: </span>
-          <span className="value">{movie.Director.name}</span>
-        </div>
-        <div className="movie-featured">
-          <span className="label">Featured: </span>
-          <span className="value">{movie.featured}</span>
-        </div>
-        <button
+        <Row>
+          <Col>
+            <div className="movie-poster">
+              <img src={movie.ImagePath} />
+            </div>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <div className="movie-title">
+              <span className="label">Title: </span>
+              <span className="value">{movie.Title}</span>
+            </div>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <div className="movie-description">
+              <span className="label">Description: </span>
+              <span className="value">{movie.Description}</span>
+            </div>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <div className="movie-genre">
+              <span className="label">Genre: </span>
+              <span className="value">{movie.Genre}</span>
+            </div>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <div className="movie-director">
+              <span className="label">Director: </span>
+              <span className="value">{movie.Director.name}</span>
+            </div>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <div className="movie-featured">
+              <span className="label">Featured: </span>
+              <span className="value">{movie.featured}</span>
+            </div>
+          </Col>
+        </Row>
+        <Button
           onClick={() => {
             onBackClick(null);
           }}
         >
           Back
-        </button>
+        </Button>
       </div>
     );
   }
