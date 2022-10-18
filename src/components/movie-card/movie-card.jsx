8180561814a545b99d19c6ae30a824f6cd9ed1f5
nickbeasley <109631653+nickbeasley@users.noreply.github.com>
2022-10-18 12:36:38 -0500

    Update movie-card.jsx
    add cross origin

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -7,10 +7,9 @@ import "./movie-card.scss";
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;
-
     return (
       <Card>
-        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Img variant="top" crossOrigin="anonymous" src={movie.ImagePath} />
         <Card.Body>
           <Card.Title>{movie.Title}</Card.Title>
           <Card.Text>{movie.Description} </Card.Text>
