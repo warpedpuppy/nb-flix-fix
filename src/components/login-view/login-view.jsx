d3f39a193cb0a79e7a44181101c54b0b08d4d412
nickbeasley <109631653+nickbeasley@users.noreply.github.com>
2022-10-22 10:50:02 -0500

    Update login-view.jsx
    update to login authorization

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,67 +1,87 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { Form, Button, Card, Col, Row } from "react-bootstrap";
+import { Form, Button, Container, Card, Col, Row } from "react-bootstrap";
 import axios from "axios";
 
 export function LoginView(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  // Declare hook for each input
+  const [usernameErr, setUsernameErr] = useState("");
+  const [passwordErr, setPasswordErr] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    axios
-      // This api url? <--------------------------------------
-      .post("https://nixflix.herokuapp.com/login", {
-        Username: username,
-        Password: password,
-      })
-      .then((response) => {
-        const data = response.data;
-        props.onLoggedIn(data);
-      })
-      .catch((e) => {
-        console.log("no such user");
-      });
+  // validate user inputs
+  const validate = () => {
+    let isReq = true;
+    if (!username) {
+      setUsernameErr("Username Required");
+      isReq = false;
+    } else if (username.length < 2) {
+      setUsernameErr("Username must be 2 characters long");
+      isReq = false;
+    }
+    if (!password) {
+      setPasswordErr("Password Required");
+      isReq = false;
+    } else if (password.length < 6) {
+      setPassword("Password must be 6 characters long");
+      isReq = false;
+    }
 
-    console.log(username, password);
-    /* Send a request to the server for authentication */
-    /* then call props.onLoggedIn(username) */
-    // props.onLoggedIn(username);
+    return isReq;
   };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const isReq = validate();
+    if (isReq) {
+      /* Send request to the server for authentication */
+      axios
+        .post("https://nixflix.herokuapp.com/login", {
+          Username: username,
+          Password: password,
+        })
+        .then((response) => {
+          const data = response.data;
+          console.log("username: ", username);
+          console.log("password: ", password);
+          console.log("data from handleLogin function", data);
+          props.onLoggedIn(data);
+        })
+        .catch((e) => {
+          console.log("Login Error", e);
+        });
+    }
+  };
+
   return (
-    <Row>
-      <Col>
-        <Card>
-          <Card.Body>
-            <Card.Title>Login</Card.Title>
-            <Form>
-              <Form.Group controlId="formUsername">
-                <Form.Label>Username: </Form.Label>
-                <Form.Control
-                  type="text"
-                  onChange={(e) => setUsername(e.target.value)}
-                  required
-                  placeholder="username"
-                />
-              </Form.Group>
+    <Form>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        {/* code added here to display validation error */}
+        {usernameErr && <p>{usernameErr}</p>}
+      </Form.Group>
 
-              <Form.Group controlId="formPassword">
-                <Form.Label>Password: </Form.Label>
-                <Form.Control
-                  type="password"
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                  placeholder="password"
-                />
-              </Form.Group>
-
-              <Button variant="primary" type="submit" onClick={handleSubmit}>
-                Submit
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {/* code added here to display validation error */}
+        {passwordErr && <p>{passwordErr}</p>}
+      </Form.Group>
+      <Button variant="primary" type="submit" onClick={handleLogin}>
+        Submit
+      </Button>
+    </Form>
   );
 }
